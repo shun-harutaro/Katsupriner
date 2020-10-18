04e4aa81b3328ce30853dd31829261deb9c4dcb6
Shun_Harutaro <n.shunshun2004@gmail.com>
2020-10-19 08:50:36 +0900

    Vuexでログインできた！

--- hosting/src/main.js
+++ hosting/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import store from './store'
 import '@/assets/css/tailwind.css'
 
 import { firestorePlugin } from 'vuefire'
@@ -25,5 +26,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
