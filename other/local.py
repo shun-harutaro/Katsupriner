bee5cace964ee44d59746e63520f26bbd2973c74
Shun_Harutaro <n.shunshun2004@gmail.com>
2020-12-01 21:03:55 +0900

    local.pyを追加

---
+++ other/local.py
@@ -0,0 +1,80 @@
+import flask
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import PyPDF2
+import linecache
+import urllib.request
+import os
+from google.cloud import firestore
+
+# get date
+year = 2020
+month = 11
+date = 30
+print(f'{year}/{month}/{date}')
+
+# get root URL
+root = "http://math.dge.toyota-ct.ac.jp/katsutani/lssn/2020/ba2/"
+
+# Project ID is determined by the GCLOUD_PROJECT environment variable
+db = firestore.Client()
+
+# scraping and get new links
+def scape():
+    for d in ['i','m']:
+        res = requests.get(f'{root}wkprntans/index-l2{d}.html')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        source = soup.find_all('a',text=f'{month}/{date}')
+        links = [url.get('href') for url in source]
+        if d=='m':
+            m=links
+        else:
+            i=links
+    print(m,i)
+    return m,i
+
+# extract score
+def correct(d):
+    for n in d:
+        num=n[4:8] #student's number
+        no=int(n[11:13]) #times
+        url = f'{root}wkprntans/'+n
+        savename = "print.pdf"
+        urllib.request.urlretrieve(url, savename)
+
+        with open(savename, "rb") as f:
+            reader = PyPDF2.PdfFileReader(f)
+            page = reader.getPage(0)
+            text = page.extractText()
+            line = text.splitlines()
+            try:
+                result = int(line[9])
+            except ValueError: #case of first submit
+                print("First")
+                result = None
+        DB(num,result,no)
+        os.remove('print.pdf')
+    return 0
+
+# save in DB
+def DB(num,result,no):
+    setData = db.collection(num).document(str(no).zfill(2))
+    last = no-1
+    if last==0:
+        pass
+    else:
+        db.collection(num).document(str(last).zfill(2)).update({u'score': result})
+    setData.set({
+        u'date': f'{year}/{month}/{date}',
+        u'score': None
+    }, merge=True)
+    return 0
+
+# main function
+def main():
+    m,i=scape()
+    correct(m)
+    correct(i)
+
+main()
