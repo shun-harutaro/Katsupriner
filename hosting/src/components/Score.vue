3460e7ecbceb1d1e8f962e8ba94d80659aea4651
Shun_Harutaro <n.shunshun2004@gmail.com>
2021-01-22 21:39:52 +0900

    script部を追加

---
+++ hosting/src/components/Score.vue
@@ -0,0 +1,32 @@
+<template>
+ <div>
+    <h1 class="font-medium">{{ name }}の第{{ post.id }}回成績表</h1>
+    <p class="text-sm">{{ post.date }}</p>
+    <button v-if="name" class="bg-indigo-700 font-semibold text-white py-2 px-4 rounded">
+     <a :href="url">模範解答へ</a>
+    </button>
+ </div>
+</template>
+
+<script>
+import { db } from '@/main'
+
+export default {
+  computed:{
+    name(){
+      return this.$store.getters["name"];
+    },
+    link: function() {
+      return function(times){
+        var Id = this.$store.getters["name"];
+        return process.env.VUE_APP_PROFESSORS_URL+'wkprntans/l'+Id.slice(0,2)+'/'+Id+'ans'+times+'.pdf'
+      }
+    }
+  },
+  firestore() {
+    return {
+      posts: db.collection(this.$store.getters["name"])
+    }
+  }
+}
+</script>
---
+++ hosting/src/views/Share.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="share">
+        <Score/>
+    </div>
+</template>
+
+<script>
+import Score from '@/components/Score'
+export default {
+    name: 'share',
+    components: {
+        Score
+    }
+}
+</script>
